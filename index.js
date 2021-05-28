4a3e8355d119a7dd9fe6e0a3bd7c93c079b023c2
UserNullified <mr.penguinczech@gmail.com>
2021-05-28 02:54:48 +0200

    Base Files, Started Loading Screen

---
+++ index.js
@@ -0,0 +1,7 @@
+function init () {
+    let loadingInfoText = ["Starting up!", "Finding Modpack JSON!", "Finishing up!"]
+    let loadingStep = 0
+    let element = document.getElementById('loadingInfoText')
+}
+
+init()
